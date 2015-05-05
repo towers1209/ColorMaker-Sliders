a827cca51953055aed1f20d98758b4d37cf439bd
Loreto E Torres <Loreto.E.Torres@gmail.com>
2015-05-05 17:57:32 -0400

    Initial Commit

---
+++ ColorMaker-Sliders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorMaker-Sliders
+//
+//  Created by Loreto E. Torres on 5/5/15.
+//  Copyright (c) 2015 towers1209. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
