d73aca5c7f97e2285ab2d83d11d72c9f6b9ca7b6
Loreto E Torres <Loreto.E.Torres@gmail.com>
2015-05-05 20:47:27 -0400

    Implementation of Lesson 1 Challenge App - Color Maker with Sliders

--- ColorMaker-Sliders/ViewController.swift
+++ ColorMaker-Sliders/ViewController.swift
@@ -5,11 +5,26 @@
 //  Created by Loreto E. Torres on 5/5/15.
 //  Copyright (c) 2015 towers1209. All rights reserved.
 //
+//  The basic concept is adapted from the exercise app ColorMaker that uses three toggle switches for turning on or off the RGB color components.
+//  So instead of simply a choice of 0 or 1 for the r, g, b color compoments in
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var greenValue: UILabel!
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var blueValue: UILabel!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +35,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func changeColorComponent(sender: AnyObject) {
+        
+        redValue.text = "\(self.redSlider.value)"
+        greenValue.text = "\(self.greenSlider.value)"
+        blueValue.text = "\(self.blueSlider.value)"
+        
+        let r: CGFloat = CGFloat(self.redSlider.value)
+        let g: CGFloat = CGFloat(self.greenSlider.value)
+        let b: CGFloat = CGFloat(self.blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        
+    }
 
 }
 
